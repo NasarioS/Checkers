5eeec39a001be328d05bc9849aa9cc74e6c000c0
Nasario S <nasario.ita@gmail.com>
2020-06-25 13:38:28 -0700

    Adding the checker pieces to the board, the pieces do not have event handlers at this point

--- src/Board.java
+++ src/Board.java
@@ -18,11 +18,26 @@ public class Board extends Application{
 		Pane root = new Pane();
 		root.setPrefSize(WIDTH * TILE_SIZE, HIGHT *TILE_SIZE);
 		root.getChildren().addAll(tGroup, pGroup);
-		
+		//(i,j) -> (x,y)
 		for(int i = 0; i < WIDTH;i++) {
 			for(int j = 0; j < HIGHT; j++) {
 				Tile tile = new Tile((i+j)%2 == 0, i,j);
 				tGroup.getChildren().add(tile);
+				
+				Piece p = null;
+				//if y is less than 3 and the tile is odd place a red piece
+				if(j < 3 && (i+j)%2 == 1 ) {
+					p = new Piece(PieceInfo.RED, i, j);
+				}
+				if(j > 5 && (i+j)%2 == 1) {
+					p = new Piece(PieceInfo.WHITE, i, j);
+				}
+				
+				if(p != null) {
+					pGroup.getChildren().add(p);
+				}
+				
+				
 			}
 		}
 		
--- src/Piece.java
+++ src/Piece.java
@@ -1,4 +1,31 @@
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
 
-public class Piece {
-
+public class Piece extends Ellipse {
+	private PieceInfo piece;
+	//x,y cords on the board
+	
+	
+	Piece(PieceInfo piece, int x, int y){
+		this.piece = piece;
+		// 0.314 is the rel rad to the tile size
+		this.setRadiusX(Board.TILE_SIZE * 0.314); 
+		this.setRadiusY (Board.TILE_SIZE * 0.314);
+		
+		this.relocate(Board.TILE_SIZE * x, Board.TILE_SIZE * y);
+		
+		if(piece == PieceInfo.RED) {
+			this.setFill(Color.RED);
+		}
+		else {
+			this.setFill(Color.WHITE);
+		}
+		
+		this.setStroke(Color.GREY);
+		this.setStrokeWidth(Board.TILE_SIZE * .05);
+		//centering piece in square
+		//trial and error dividing by 6 nearly centers the piece
+		this.setTranslateX(Board.TILE_SIZE / 6);
+		this.setTranslateY(Board.TILE_SIZE / 6);
+	}
 }
