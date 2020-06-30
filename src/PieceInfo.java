d01cc25f018c08e30e0da2277b0e8adc92218fa9
Nasario S <nasario.ita@gmail.com>
2020-06-29 23:12:47 -0700

    Changed red to -1 to more easily check if a move is possible

--- src/PieceInfo.java
+++ src/PieceInfo.java
@@ -1,5 +1,5 @@
 public enum PieceInfo {
-  WHITE(1), RED(0); 
+  WHITE(1), RED(-1); 
   int move;
 	//the direction of which the piece can move, 1 for up and 0 for down, red will be at the top of the board
   PieceInfo(int move){
